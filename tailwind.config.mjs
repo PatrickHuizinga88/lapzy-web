e9979a0e4bbdd09a0886bb391c5da7aee82a172e
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2024-05-25 13:47:30 +0200

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			center: true,
+			padding: "1.5rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+			display: ["Anton", "sans-serif"],
+			logo: ["Tomorrow", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+			},
+			borderRadius: {
+				xl: "calc(var(--radius) + 4px)",
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+		},
+	},
+	plugins: [],
+}
