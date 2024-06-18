8765b3764dc7b49b4be491cabd7decb4931fbcaa
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2024-06-18 21:56:38 +0200

    Added sitemap and robots file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://lapzy.nl',
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: ['https://app.lapzy.nl'],
+    })
+  ]
 });
